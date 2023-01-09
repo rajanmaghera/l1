f969c21824c60831e98e9af7000fa19c4b35b8f5
Rajan Maghera <rmaghera@ualberta.ca>
2023-01-09 15:46:58 -0700

    Add printing of self code

--- version.py
+++ version.py
@@ -1,2 +1,9 @@
 from importlib.metadata import version
+import requests
+
 print("The version is", version('requests'))
+
+# pull script from GH
+
+r = requests.get("https://raw.githubusercontent.com/rajanmaghera/l1/main/version.py")
+print(r.text)
